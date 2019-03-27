e31393777b676e81373bdbc201c2763ec598c31a
Delagardi <delagardi@ymail.com>
2019-03-27 04:12:16 +0200

    First commit. Installd Material UI, delete some useless files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <CssBaseline>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <AccessAlarm/>
+      </CssBaseline>
     );
   }
 }
